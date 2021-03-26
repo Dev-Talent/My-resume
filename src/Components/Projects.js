d4f8aa8e9e0a1b9fa5b1e103606bb2c8a313cbe8
Talent Cihizema <tchizema@gmail.com>
2021-03-26 15:58:12 +0200

    add

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,7 +3,7 @@ import "./Projects.css";
 export default function Projects() {
     return (
         <div className="bton">
-            <a href='https://immense-ridge-34152.herokuapp.com/' className="float">
+            <a href='https://talent-todolist.herokuapp.com/' className="float">
     <button className="projects">Projects</button>
   </a>
         </div>
