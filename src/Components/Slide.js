baaf32758c69d5717c08c2e3f06f3f8deef76e6b
Talent Cihizema <tchizema@gmail.com>
2021-03-26 15:28:24 +0200

    add

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 export default function Slider({title, img, rating}) {
     return (
         <div className="product">
+
         <div className="product_info">
             <p className="title">{title}</p>
             <div className="product_rating">
@@ -17,10 +18,6 @@ export default function Slider({title, img, rating}) {
             </div>
         </div>
            <img src={img} alt=""></img> 
-           <Link to="/services">
-           
-           </Link>
-           
         </div>
     )
 }
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import "./Slider.css";
-import {Link} from "react-router-dom";
-
+import { Link } from 'react-router-dom';
 export default function Slider({title, img, rating}) {
     return (
 
         <div className="product">
-        <Link to="/services">
+        <Link style={{ textDecoration: 'none' }} to="/services">
         <div className="product_info">
        
             <p className="title">{title}</p>
@@ -20,14 +19,15 @@ export default function Slider({title, img, rating}) {
             </div>
             </div>
             <div>
+           
         <img src={img} alt=""></img> 
+        
         </div>
           <div>
           
          {/*  <button className="btn2">Projects</button>*/}
            
           </div>
-           
           </Link>
         </div>
         
