0fc0f5006048f2ba67486c6982f2035a39e70f57
Talent Cihizema <tchizema@gmail.com>
2021-03-28 19:32:28 +0200

    add

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import "./Slider.css";
-import {Link} from "react-router-dom";
+import "./Slide.css"
+//import {Link} from "react-router-dom";
 
 export default function Slider({title, img, rating}) {
     return (
-        <div className="product">
+        <div className="produc">
 
         <div className="product_info">
             <p className="title">{title}</p>
             <div className="product_rating">
-            {Array(rating)
-            .fill()
-            .map((_, i) => (
-                <p>⭐</p>
-            ))}
-                
+          
             </div>
         </div>
            <img src={img} alt=""></img> 
+           
         </div>
+       
     )
 }
